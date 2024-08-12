11e514b06e6f2526d42357c022ee09d81f83e719
s1ryaniu <ryan.niu@cerence.com>
2024-08-12 19:34:21 +0800

    feat: add clang-format and cmake-format to normalize spdlog project

--- demo/spdlog_Test/spdlog_Test.cpp
+++ demo/spdlog_Test/spdlog_Test.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
 #include "funset.hpp"
+
+#include <iostream>
 
 int main()
 {
-	int ret = test_spdlog_console();
-	// auto aysncRet = test_spdlog_async();
+    int ret = test_spdlog_console();
+    // auto aysncRet = test_spdlog_async();
 
-	if (ret == 0) fprintf(stdout, "========== test success ==========\n");
-	else fprintf(stderr, "########## test fail ##########\n");
+    if (ret == 0)
+        fprintf(stdout, "========== test success ==========\n");
+    else
+        fprintf(stderr, "########## test fail ##########\n");
 
-	return 0;
+    return 0;
 }
-
